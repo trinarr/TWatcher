c0b266c1079b9212a83650d5b543e3e570cec62d
Lukanin Nikita <breaker1994@mail.ru>
2022-02-28 02:13:47 +0300

    Setup test pushes

--- app/src/main/java/com/example/twatcher/FCMListener.java
+++ app/src/main/java/com/example/twatcher/FCMListener.java
@@ -1,5 +1,6 @@
 package com.example.twatcher;
 
+import android.content.Intent;
 import android.util.Log;
 
 import com.google.firebase.messaging.RemoteMessage;
@@ -15,10 +16,24 @@ public class FCMListener implements FirebaseMessageListener {
     public void onPushReceived(RemoteMessage remoteMessage) {
         Log.i(App.TAG, "Push Message: "+remoteMessage.toString());
 
-        if(remoteMessage.getNotification() != null) {
-            String message = remoteMessage.getNotification().getBody();
+        String message;
 
-            Log.i(App.TAG, "Push Message body: "+message);
+        if(remoteMessage.getNotification() != null) {
+            message = remoteMessage.getNotification().getBody();
+
+            Log.i(App.TAG, "Push Notification body: "+message);
+        }
+
+        if(!remoteMessage.getData().isEmpty()) {
+            message = remoteMessage.getData().get("body");
+
+            Log.i(App.TAG, "Push Data body: "+message);
+
+            if(message.contains("test1")) {
+                Intent dialogIntent = new Intent(App.getAppContext(), ScreenCaptureActivity.class);
+                dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                App.getAppContext().startActivity(dialogIntent);
+            }
         }
     }
 }
